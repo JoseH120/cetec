7c1326df47dff11b18aff7f4d4944be2a7cc988a
Jose Hernandez <reyes_7374@outlook.es>
2024-11-05 20:34:05 -0600

    adding imagebackground

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Main Page</h1>
+  <div
+    class="bg-image"
+    style="
+      background-image: url('http://localhost/sistema/uploads/fondo/cetec_home.jpg');
+      height: 100vh;
+    "
+  ></div>
 </template>
+<style scoped>
+.bg-image {
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+
+@media (max-width: 768px) {
+  .bg-image {
+    min-height: 30vh;
+  }
+}
+</style>
